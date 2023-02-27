36f4635c3b4ad0efd004a476fd6f83f68973dc22
nguyenchanhthong2102 <124759370+nguyenchanhthong2102@users.noreply.github.com>
2023-02-27 19:49:46 +0700

    update

---
+++ main.js
@@ -0,0 +1,34 @@
+
+function HandleVideo(){
+    var videos=document.querySelectorAll('.video-item');
+    var iframe=document.querySelector('#iframe');
+    var videoplay=document.querySelector('.videoplay')
+    var popupVideo=document.querySelector('.popup-video');
+    var button=document.querySelector('.close');
+    videos.forEach(function(video){
+        video.addEventListener('click',function(){
+            popupVideo.style.visibility='visible';
+            iframe.style.visibility='visible';
+            let dataID=video.getAttribute('data-video-src');
+            videoplay.setAttribute('src',`https://www.youtube.com/embed/${dataID}?autoplay=1`)
+        })
+   
+        })
+    function closeVideo(){
+
+        popupVideo.style.visibility='hidden';
+        iframe.style.visibility='hidden';
+        videoplay.setAttribute('src','');
+    }
+    iframe.addEventListener('click',function(){
+        closeVideo();
+    })
+    button.addEventListener('click',function(){
+        closeVideo();
+    })
+}
+HandleVideo();
+
+
+
+
